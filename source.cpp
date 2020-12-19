fd7e4986d62b0d1d96cfc5814e28119a3ba7bb32
harislye <harislye@gmail.com>
2020-12-19 11:34:49 +0800

    program with adder quiz

--- source.cpp
+++ source.cpp
@@ -2,9 +2,13 @@
 using namespace std;
 
 int main() {
+	int x;
 	cout << "\n Hello world ";
 	cout << " \n I like Git" ;
 	cout << " I like Github" ;
+	
+	cout << " what is 1+ 4 ";
+	cin >> x;
 	
 	return 1;
 }
